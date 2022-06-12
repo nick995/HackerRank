1bd033b510d7c2766068a0cba37f62f4f498fac4
Min-Gyu <mingyu995@gmail.com>
2022-06-12 13:50:23 -0600

    solved careful the count

--- src/Algorithm/temp.java
+++ src/Algorithm/temp.java
@@ -7,45 +7,28 @@ import java.util.List;
 public class temp {
 	public static void main(String[] args) {
 
-		List<Integer> temp = new ArrayList<>(5);
+		List<Integer> temp = new ArrayList<>(4);
 
 		temp.add(1);
 		temp.add(100);
 		temp.add(10);
-		temp.add(1000);
-		temp.add(10000);
+		temp.add(100);
 
 		
-		int[] tempArray = new int[5];
+		int max = temp.get(0);
+		int count = 0;
 
 		for (int i = 0; i < temp.size(); i++) {
-			tempArray[i] = temp.get(i);
+			if (max < temp.get(i)) {
+				max = temp.get(i);
+				count = 1;
+			} else if (max == temp.get(i)) {
+				count++;
+			}
 		}
+		System.out.println(max);
 		
-		for(int i : tempArray) {
-			System.out.print(i + " ");
-		}
-
-		System.out.println();
-		Arrays.sort(tempArray);
-		
-		for(int i : tempArray) {
-			System.out.print(i + " ");
-		}
-				
-		int min = 0;
-		int max = 0;
-
-		for (int i = 0; i < temp.size() - 1; i++) {
-			min += tempArray[i];
-		}
-
-		for (int i = 1; i < temp.size(); i++) {
-			max += tempArray[i];
-		}
-
-//		System.out.println(min + " " + max);
-
+		System.out.println(count);
 	}
 
 }
