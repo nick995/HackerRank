63ad3f6e38b07aa1da2efb0a960c114a36db9658
Min-Gyu <mingyu995@gmail.com>
2022-06-21 16:15:45 -0600

    solved

--- src/Algorithm/AppleAndOrangeExitFullScreenView.java
+++ src/Algorithm/AppleAndOrangeExitFullScreenView.java
@@ -12,57 +12,75 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'countApplesAndOranges' function below.
-     *
-     * The function accepts following parameters:
-     *  1. INTEGER s
-     *  2. INTEGER t
-     *  3. INTEGER a
-     *  4. INTEGER b
-     *  5. INTEGER_ARRAY apples
-     *  6. INTEGER_ARRAY oranges
-     */
+	/*
+	 * Complete the 'countApplesAndOranges' function below.
+	 *
+	 * The function accepts following parameters: 1. INTEGER s 2. INTEGER t 3.
+	 * INTEGER a 4. INTEGER b 5. INTEGER_ARRAY apples 6. INTEGER_ARRAY oranges
+	 */
 
-    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
-    // Write your code here
+	public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+		// Write your code here
 
-    }
+		List<Integer> apple = new ArrayList<Integer>(apples);
+		List<Integer> orange = new ArrayList<Integer>(oranges);
+
+		int appleCount = 0;
+		int orangeCount = 0;
+
+		for (int i : apple) {
+			i += a;
+			if (i >= s && i <= t) {
+				appleCount++;
+			}
+
+		}
+
+		for (int i : orange) {
+
+			i += b;
+
+			if (i >= s && i <= t) {
+				orangeCount++;
+			}
+		}
+
+		System.out.println(appleCount + "\n" + orangeCount);
+
+	}
 
 }
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+		String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        int s = Integer.parseInt(firstMultipleInput[0]);
+		int s = Integer.parseInt(firstMultipleInput[0]);
 
-        int t = Integer.parseInt(firstMultipleInput[1]);
+		int t = Integer.parseInt(firstMultipleInput[1]);
 
-        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+		String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        int a = Integer.parseInt(secondMultipleInput[0]);
+		int a = Integer.parseInt(secondMultipleInput[0]);
 
-        int b = Integer.parseInt(secondMultipleInput[1]);
+		int b = Integer.parseInt(secondMultipleInput[1]);
 
-        String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+		String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        int m = Integer.parseInt(thirdMultipleInput[0]);
+		int m = Integer.parseInt(thirdMultipleInput[0]);
 
-        int n = Integer.parseInt(thirdMultipleInput[1]);
+		int n = Integer.parseInt(thirdMultipleInput[1]);
 
-        List<Integer> apples = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+		List<Integer> apples = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
 
-        List<Integer> oranges = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+		List<Integer> oranges = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
 
-        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+		Result.countApplesAndOranges(s, t, a, b, apples, oranges);
 
-        bufferedReader.close();
-    }
+		bufferedReader.close();
+	}
 }
