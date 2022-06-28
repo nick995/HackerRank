7b90c8f9d987e4bf33f8659c4384a641a5e0385a
Min-Gyu <mingyu995@gmail.com>
2022-06-28 16:35:59 -0600

    solved

--- src/Algorithm/practice.java
+++ src/Algorithm/practice.java
@@ -1,22 +1,42 @@
 package Algorithm;
 
-import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class practice {
 	public static void main(String[] args) {
-		
-		BigInteger temp = new BigInteger("1");
 
-		for(int i=1; i<10; i++) {
-			
-			temp = temp.multiply(BigInteger.valueOf(i));
-			
-			System.out.println(temp);
-			
+		List<Integer> s = new ArrayList<Integer>();
+		int k = 3;
+		s.add(1);
+		s.add(7);
+		s.add(2);
+		s.add(4);
+
+		// ==============================================
+
+		int[] bucket = new int[k];
+		int answer = 0;
+		// place to the bucket.
+		for (int i : s) {
+			bucket[i % k]++;
 		}
 
+		for (int i = 0; i <= bucket.length / 2; i++) {
+
+			if ((k - i) % k == i) {
+				if (bucket[i] > 0) {
+					answer++;
+				}
+			} else {
+				if (bucket[i] > bucket[(k - i) % k]) {
+					answer += bucket[i];
+				} else {
+					answer += bucket[(k - i) % k];
+				}
+			}
+		}
+		System.out.println(answer);
 	}
 }
