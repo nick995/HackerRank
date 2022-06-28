ab028fa713e7603f78bd9718c8261911c49694d8
Min-Gyu <mingyu995@gmail.com>
2022-06-28 14:15:45 -0600

    solved why it's medium level?

--- src/Algorithm/practice.java
+++ src/Algorithm/practice.java
@@ -1,59 +1,22 @@
 package Algorithm;
 
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class practice {
 	public static void main(String[] args) {
-
 		
-		
-		List<Integer> ranked = new ArrayList<Integer>();
-		List<Integer> player = new ArrayList<Integer>();
+		BigInteger temp = new BigInteger("1");
 
-
-		ranked.add(100);
-		ranked.add(100);
-		ranked.add(50);
-		ranked.add(40);
-		ranked.add(40);
-		ranked.add(20);
-		ranked.add(10);
-
-		player.add(5);
-		player.add(25);
-		player.add(50);
-		player.add(120);
-		
-		//==============================================
-		ArrayList<Integer> answer = new ArrayList<Integer>();
-
-		
-		List<Integer> temp = ranked.stream().distinct().collect(Collectors.toList());
-				
-		Collections.sort(temp);
-		
-		int stopTemp = 0;
-		
-		int max = temp.get(temp.size()-1);
-		for (int i = 0; i < player.size(); i++) {
-			for (int j = stopTemp; j < temp.size(); j++) {
-				if (player.get(i) < temp.get(j)) {
-					answer.add(temp.size() - j + 1);
-					stopTemp = j;
-					break;
-				}else if(j >= temp.size()-1) {
-					answer.add(1);
-				}
-				
-				
-
-			}
-
+		for(int i=1; i<10; i++) {
+			
+			temp = temp.multiply(BigInteger.valueOf(i));
+			
+			System.out.println(temp);
+			
 		}
-		
-		System.out.println(answer);
 
 	}
 }
