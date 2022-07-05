71ae797a4c7759c1e45784058ccb9a2db68ff9fa
Min-Gyu <mingyu995@gmail.com>
2022-07-04 23:17:20 -0600

    getOriginal working fine

--- src/Algorithm/practice.java
+++ src/Algorithm/practice.java
@@ -5,38 +5,126 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class practice {
+
+	public static int originalPossible = 0;
+
 	public static void main(String[] args) {
 
-		List<Integer> s = new ArrayList<Integer>();
+		int n = 8;
 		int k = 3;
-		s.add(1);
-		s.add(7);
-		s.add(2);
-		s.add(4);
 
-		// ==============================================
+		int r_q = 4;
+		int c_q = 4;
 
-		int[] bucket = new int[k];
-		int answer = 0;
-		// place to the bucket.
-		for (int i : s) {
-			bucket[i % k]++;
+		int[][] obstacles = new int[n][n];
+
+		obstacles[4][2] =
+
+				getOriginal(n, r_q, c_q);
+
+	}
+
+	public static int getOriginal(int square, int x, int y) {
+
+		int n = square;
+		int r_q = x;
+		int c_q = y;
+
+		int rowSum = 0;
+		int colSum = 0;
+		int quadrant1 = 0;
+		int quadrant2 = 0;
+		int quadrant3 = 0;
+		int quadrant4 = 0;
+
+		rowSum = n - 1;
+		colSum = n - 1;
+
+		System.out.println("quadrant 1 ");
+		// quadrant1
+		for (int x1 = r_q + 1, y1 = c_q + 1; x1 < n || y1 < n; x1++, y1++) {
+
+			System.out.println(x1 + " " + y1);
+
+			quadrant1++;
 		}
 
-		for (int i = 0; i <= bucket.length / 2; i++) {
+		System.out.println("quadrant 1 possible = " + quadrant1);
+		System.out.println();
 
-			if ((k - i) % k == i) {
-				if (bucket[i] > 0) {
-					answer++;
-				}
-			} else {
-				if (bucket[i] > bucket[(k - i) % k]) {
-					answer += bucket[i];
-				} else {
-					answer += bucket[(k - i) % k];
-				}
+		System.out.println("quadrant 2 ");
+		// quadrant2
+		for (int x2 = r_q + 1, y2 = c_q - 1; x2 < n || y2 > 1; x2++, y2--) {
+
+			System.out.println(x2 + " " + y2);
+
+			quadrant2++;
+
+		}
+		System.out.println("quadrant 2 possible = " + quadrant2);
+
+		System.out.println("quadra 3 ");
+
+		// quadrant3
+		for (int x3 = r_q - 1, y3 = c_q - 1; x3 > 1 || y3 > 1; x3--, y3--) {
+
+			System.out.println(x3 + " " + y3);
+
+			quadrant3++;
+
+		}
+
+		System.out.println("quadrant 3 possible = " + quadrant3);
+
+		System.out.println("quadra 4 ");
+
+		// quadrant3
+		for (int x4 = r_q - 1, y4 = c_q + 1; x4 > 1 || y4 < n; x4--, y4++) {
+
+			System.out.println(x4 + " " + y4);
+
+			quadrant4++;
+		}
+
+		System.out.println("quadrant 4 possible = " + quadrant4);
+
+		originalPossible = rowSum + colSum + quadrant1 + quadrant2 + quadrant3 + quadrant4;
+
+		System.out.println(originalPossible);
+
+		return originalPossible;
+
+	}
+
+	public static int obstacleCount(int n, int r_q, int c_q, int obstacleNum, List<List<Integer>> obstacles) {
+
+		int row = 0;
+		int col = 0;
+
+		int blockCount = 0;
+
+		int temp =0;
+		
+		for (int i = 0; i < obstacles.get(0).size(); i++) {
+			row = obstacles.get(i).get(0);
+			col = obstacles.get(i).get(1);
+
+			// case 1.
+			if (row == r_q) {
+				blockCount += Math.min(col - 0, col - n);
 			}
+
+			if (col == c_q) {
+				blockCount += Math.min(row - 0, row - n);
+			}
+			
+			if(Math.abs(row-r_q) + Math.abs(col-c_q) % 2 == 0 ) {
+				
+			}
+
 		}
-		System.out.println(answer);
+
+		return obstacleNum;
+
 	}
 }
