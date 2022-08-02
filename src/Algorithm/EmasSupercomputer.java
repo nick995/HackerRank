78123149d72190fe4518b74d172ae346040e1d5f
Min-Gyu <mingyu995@gmail.com>
2022-08-02 17:16:03 -0600

    almost done cheer up

---
+++ src/Algorithm/EmasSupercomputer.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'twoPluses' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING_ARRAY grid as parameter.
+     */
+
+    public static int twoPluses(List<String> grid) {
+    // Write your code here
+    	
+    	
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<String> grid = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        int result = Result.twoPluses(grid);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- src/Algorithm/practice.java
+++ src/Algorithm/practice.java
@@ -5,6 +5,8 @@ import java.util.Map.Entry;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Collectors;
+import java.awt.Point;
+import java.lang.*;
 
 public class practice {
 
@@ -12,136 +14,193 @@ public class practice {
 
 	public static void main(String[] args) {
 
-		int n = 5;
-
 		List<String> grid = new ArrayList<String>();
-		grid.add(".......");
-		grid.add("...O...");
-		grid.add("....O..");
-		grid.add(".......");
-		grid.add("OO.....");
-		grid.add("OO.....");
+		
+		//TEST 1
+		grid.add("BBBGBGBBB");
+		grid.add("BBBGBGBBB");
+		grid.add("BBBGBGBBB");
+		grid.add("GGGGGGGGG");
+		grid.add("BBBGBGBBB");
+		grid.add("BBBGBGBBB");
+		grid.add("GGGGGGGGG");
+		grid.add("BBBGBGBBB");
+		grid.add("BBBGBGBBB");
+		grid.add("BBBGBGBBB");
+		
+		
+//		grid.add("BBBGBBB");
+//		grid.add("BBBGBBB");
+//		grid.add("BGGGGGB");
+//		grid.add("BBBGBBB");
+//		grid.add("BBBGBBB");
+		ArrayList<StringBuilder> temp = new ArrayList<StringBuilder>();
 
-		// =============================================================
-
-		ArrayList<String> answer = new ArrayList<String>();
-
-		List<StringBuilder> temp = new ArrayList<StringBuilder>();
-
-		StringBuilder tempSb = new StringBuilder();
-
-		String tempS = "";
-
-		int cycle = 0;
-		// nothing happen
-		if (n == 1) {
-			return grid;
-		}
-
-		// for 2,3,4 needed.
-		for (int i = 0; i < grid.get(0).length(); i++) {
-			tempS += "O";
-		}
-
-		// for 2,3,4 needed.
-		for (int i = 0; i < grid.size(); i++) {
-			tempSb = new StringBuilder(tempS);
-			temp.add(tempSb);
-		}
-
-		// first case all bomb.
-		if (n % 2 == 0) {
-			for (StringBuilder sb : temp) {
-				answer.add(sb.toString());
-			}
-			return answer;
-		}
+		StringBuilder sbTemp = new StringBuilder("");
+		
+		
+		HashMap<Point, SquareData> sqData = new HashMap<Point, SquareData>();
+		
 
 		for (int i = 0; i < grid.size(); i++) {
+			sbTemp = new StringBuilder("");
 			for (int j = 0; j < grid.get(0).length(); j++) {
-				if (grid.get(i).charAt(j) == 'O') {
-
-					// middle
-					temp.get(i).setCharAt(j, '.');
-
-					// up side
-					if (i - 1 >= 0) {
-						temp.get(i - 1).setCharAt(j, '.');
-					} // left side
-					if (j - 1 >= 0) {
-						temp.get(i).setCharAt(j - 1, '.');
-					} // right side
-					if (j + 1 < grid.get(0).length()) {
-						temp.get(i).setCharAt(j + 1, '.');
-					} // down side.
-					if (i + 1 < grid.size()) {
-						temp.get(i + 1).setCharAt(j, '.');
-					}
+				if (grid.get(i).charAt(j) == 'B') {
+					sbTemp.append("9");
+				} else {
+					sbTemp.append("0");
 				}
 			}
+			temp.add(sbTemp);
 		}
 
-		for (StringBuilder sb : temp) {
-			answer.add(sb.toString());
-		}
+		Point top = new Point();
+		Point left = new Point();
+		Point right = new Point();
+		Point bottom = new Point();
+		Point main = new Point();
 
-		if (n % 4 == 3) {
-			return answer;
-		} else if (n % 4 == 1) {
-			temp = explosionCalculate(answer, 1);
-			answer.clear();
-			for (StringBuilder sb : temp) {
-				answer.add(sb.toString());
+		int cycle = 0;
+		
+		int numberth = 0;
+
+		boolean available = true;
+		// row
+		for (int y = 0; y < grid.size(); y++) {
+			// col
+			for (int x = 0; x < grid.get(0).length(); x++) {
+				// initalizing.
+				cycle = 0;
+				if (grid.get(y).charAt(x) == 'G') {
+
+					// initalize
+					main = new Point(x, y);
+					
+					do {
+						// checking square
+						available = true;
+						
+						cycle++;
+
+						// top side
+						if (y - cycle >= 0 && grid.get(y - cycle).charAt(x) == 'G') {
+							top = new Point(x, y - cycle);
+						} else {
+							available = false;
+							break;
+						}
+
+						// bottom side
+						if (y + cycle < grid.size() && grid.get(y + cycle).charAt(x) == 'G') {
+							bottom = new Point(x, y + cycle);
+						} else {
+							available = false;
+							break;
+						}
+						// left side
+						if (x - cycle >= 0 && grid.get(y).charAt(x - cycle) == 'G') {
+							left = new Point(x - cycle, y);
+						} else {
+							available = false;
+							break;
+						}
+						// right side
+						if (x + cycle < grid.get(0).length() && grid.get(y).charAt(x + cycle) == 'G') {
+							right = new Point(x + cycle, y);
+						} else {
+							available = false;
+							break;
+						}
+
+						if (available == true) {
+							
+							SquareData data = new SquareData();
+							
+							data.pointCord.add(top);
+							data.pointCord.add(bottom);
+							data.pointCord.add(left);
+							data.pointCord.add(right);
+							data.pointCord.add(main);
+							
+							data.squareSize = cycle*4 + 1;
+							
+							data.main = main;
+						
+							sqData.put(main, data);
+							
+//							System.out.println(main.x + " " + main.y + " available = " + available + " CYCLE = " +  cycle);
+														
+							
+						}else {
+							cycle--;		
+						}
+
+					} while (available = true);
+
+				}
 			}
 		}
 		
-		return answer;
+		//almost done.
+		for(Map.Entry<Point, SquareData> sq : sqData.entrySet() ) {
+						
+		}
+				
+		
+	}
+}
 
+class SquareData {
 
+	int squareSize;
+
+	ArrayList<Point> pointCord;
+
+	boolean duplicated;
+	
+	Point main;
+
+	public SquareData() {
+
+		main = new Point();
+		
+		squareSize = 0;
+
+		pointCord = new ArrayList<Point>();
+
+		duplicated = false;
 	}
 
-	public static List<StringBuilder> explosionCalculate(List<String> grid, int n) {
-
-		ArrayList<StringBuilder> temp = new ArrayList<StringBuilder>();
-
-		StringBuilder tempSb = new StringBuilder();
-
-		String tempS = "";
-
-		for (int i = 0; i < grid.get(0).length(); i++) {
-			tempS += "O";
-		}
-
-		for (int i = 0; i < grid.size(); i++) {
-			tempSb = new StringBuilder(tempS);
-			temp.add(tempSb);
-		}
-		for (int i = 0; i < grid.size(); i++) {
-			for (int j = 0; j < grid.get(0).length(); j++) {
-
-				if (grid.get(i).charAt(j) == 'O') {
-
-					// middle
-					temp.get(i).setCharAt(j, '.');
-
-					// up side
-					if (i - 1 >= 0) {
-						temp.get(i - 1).setCharAt(j, '.');
-					} // left side
-					if (j - 1 >= 0) {
-						temp.get(i).setCharAt(j - 1, '.');
-					} // right side
-					if (j + 1 < grid.get(0).length()) {
-						temp.get(i).setCharAt(j + 1, '.');
-					} // down side.
-					if (i + 1 < grid.size()) {
-						temp.get(i + 1).setCharAt(j, '.');
-					}
-				}
-			}
-		}
-
-		return temp;
-
+	public Point getMainPoint() {
+		return main;
 	}
+
+	public int getSquareSize() {
+		return squareSize;
+	}
+
+	public ArrayList<Point> getPointCord() {
+		return pointCord;
+	}
+
+	public boolean getDuplicated() {
+		return duplicated;
+	}
+
+	public void setNumberth(Point m) {
+		this.main = m;
+	}
+
+	public void setSquareSize(int n) {
+		this.squareSize = n;
+	}
+
+	public void setPointCord(ArrayList<Point> p) {
+		this.pointCord = p;
+	}
+
+	public void setDuplicated(boolean dup) {
+		this.duplicated = dup;
+	}
+
 }
