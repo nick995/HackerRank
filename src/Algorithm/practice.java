09bc82e2fe8e770f0b76f0bfd0d8ddfa624ba400
Min-Gyu <mingyu995@gmail.com>
2022-08-10 17:12:25 -0600

    temp

--- src/Algorithm/practice.java
+++ src/Algorithm/practice.java
@@ -15,48 +15,98 @@ public class practice {
 		List<Integer> arr = new ArrayList<Integer>();
 
 		arr.add(1);
-		arr.add(5);
 		arr.add(4);
 		arr.add(3);
 		arr.add(2);
+		arr.add(5);
 		arr.add(6);
+		
+		//Test case 3
+//		arr.add(1);
+//		arr.add(5);
+//		arr.add(4);
+//		arr.add(3);
+//		arr.add(2);
+//		arr.add(6);
+		
+		
+		//Test case 2 
+		
+//		arr.add(3);
+//		arr.add(1);
+//		arr.add(2);
 
 		ArrayList<Integer> list = new ArrayList<Integer>(arr);
 
+		ArrayList<Boolean> temp = new ArrayList<Boolean>();
 		int targetInt = list.get(0);
 
-		int index = 0;
+		temp.add(true);
+
+		int first = 0;
+		int last = 0;
+		
+		boolean flag = false;
+		
+
+		String option = "";
 
 		boolean availaible = false;
 
 		for (int i = 1; i < list.size(); i++) {
 
-			if (list.get(i) > targetInt) {
-				availaible = true;
-				targetInt = list.get(i);
+			if (list.get(i) > list.get(i-1)) {
+				temp.add(true);
+				
+				
+				
+				if(flag == true) {
+					last = i;
+					flag = false;
+				}
+
 			} else {
 				availaible = false;
-			}
-
-			System.out.println("index = " + i + " avail =" + availaible);
-
-			if (availaible = false) {
-
-				while (availaible = false) {
-
-					if (list.get(i) > targetInt) {
-						availaible = true;
-						targetInt = list.get(i);
-					} else {
-						availaible = false;
-					}
-					System.out.println("index = " + i + " avail =" + availaible);
-
+				
+				if(flag == false) {
+					first = i-1;
+					flag = true;
 				}
+				
+				temp.add(false);
+				
+				
+				
 			}
 
 		}
 
+		for (boolean t : temp) {
+			System.out.println(t);
+		}
+		
+		System.out.println(first);
+		
+		System.out.println(last);
+		
+		int tCount = 0;
+		int fCount = 0;
+		
+		for(int i= first; i<last; i++) {
+			if(temp.get(i) == false) {
+				fCount++;
+			}else {
+				tCount++;
+			}
+		}
+		
+		System.out.println(fCount);
+		System.out.println(tCount);
+		
+	}
+	
+	public static void swapable(List<Integer> arr, int first, int last) {
+		
 	}
 
 }
